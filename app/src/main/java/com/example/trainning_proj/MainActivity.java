4d19ee9dbe385aa01fd4d9091360366c5ecf96e4
3bmotalb <mmgamal571@gmail.com>
2020-09-15 19:50:45 +0200

    all passwords can hide and show

--- app/src/main/java/com/example/trainning_proj/MainActivity.java
+++ app/src/main/java/com/example/trainning_proj/MainActivity.java
@@ -11,31 +11,48 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText ed1_pass;
-    ImageView show;
-    static Boolean check=false;
+    EditText et1_pass, et1_repass;
+    ImageView show, show2;
+    static Boolean check1 = false, check2 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ed1_pass=findViewById(R.id.et1_pass);
-        show=findViewById(R.id.show);
+        et1_pass = findViewById(R.id.et1_pass);
+        et1_repass = findViewById(R.id.et1_rePass);
+        show = findViewById(R.id.show);
         show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(check==false)
-                {
-                    ed1_pass.setInputType(InputType.TYPE_CLASS_TEXT);
+                // to make password in password edit text show and hide
+                if (check1 == false) {
+                    et1_pass.setInputType(InputType.TYPE_CLASS_TEXT);
                     show.setImageResource(R.drawable.ic_baseline_visibility_24);
-                    check=true;
+                    check1 = true;
 
-                }
-                else
-                {
-                    ed1_pass.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    et1_pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                     show.setImageResource(R.drawable.ic_baseline_visibility_off_24);
-                    check=false;
+                    check1 = false;
+                }
+            }
+        });
+        show2 = findViewById(R.id.show2);
+        show2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // to make password in re-enter password  edit text show and hide
+
+                if (check2 == false) {
+                    et1_repass.setInputType(InputType.TYPE_CLASS_TEXT);
+                    show2.setImageResource(R.drawable.ic_baseline_visibility_24);
+                    check2 = true;
+
+                } else {
+                    et1_repass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    show2.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                    check2 = false;
                 }
             }
         });
--- app/src/main/java/com/example/trainning_proj/SignIn.java
+++ app/src/main/java/com/example/trainning_proj/SignIn.java
@@ -4,25 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignIn extends AppCompatActivity {
     Button btn_login;
+    EditText et2_pass;
+    ImageView show;
+    static Boolean check1 = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-        btn_login=findViewById(R.id.btn2_signUp);
+        et2_pass = findViewById(R.id.et2_pass);
+        btn_login = findViewById(R.id.btn2_signUp);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(SignIn.this,MainActivity.class);
+                Intent i = new Intent(SignIn.this, MainActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+        show = findViewById(R.id.show3);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // to make password in password edit text show and hide
+                if (check1 == false) {
+                    et2_pass.setInputType(InputType.TYPE_CLASS_TEXT);
+                    show.setImageResource(R.drawable.ic_baseline_visibility_24);
+                    check1 = true;
+
+                } else {
+                    et2_pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    show.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                    check1 = false;
+                }
             }
         });
     }
