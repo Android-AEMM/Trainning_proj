6cf6c6a703dea49231657f07b67e5607f56cd8c0
3bmotalb <mmgamal571@gmail.com>
2020-09-20 16:10:19 +0200

    Connect the json file to the project

--- app/src/main/java/com/example/trainning_proj/Exchange_Rates.java
+++ app/src/main/java/com/example/trainning_proj/Exchange_Rates.java
@@ -3,22 +3,31 @@ package com.example.trainning_proj;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import java.util.ArrayList;
 
-public class Exchange_Rates extends AppCompatActivity {
+public class Exchange_Rates extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<String>>{
 
-    private ListView lv;
-    private Button btn;
-    private EditText txt;
+     ListView lv;
+
+    ProgressBar progressBar;
 
     TextView item;
+    static ArrayList<String> currency =new ArrayList<>();
+    private static String CURRENCY_URL = "https://jsonware.com/json/6b73abddc0812d1ef8e3632f5d79c6c2.json";
 
 
 
@@ -28,29 +37,44 @@ public class Exchange_Rates extends AppCompatActivity {
         setContentView(R.layout.activity_exchange__rates);
 
         lv=findViewById(R.id.listView);
-        btn=findViewById(R.id.Enter);
-        txt=findViewById(R.id.name);
+
         item=findViewById(R.id.list_item);
-        final ArrayList<String> names =new ArrayList<>();
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String n=txt.getText().toString();
-
-                Custom_Adapter adapter=new Custom_Adapter(Exchange_Rates.this,android.R.layout.simple_list_item_1,names);
-                lv.setAdapter(adapter);
-
-                names.add(n);
+        progressBar = findViewById(R.id.pb_progress);
 
 
-
-            }
-        });
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.initLoader(0, null, Exchange_Rates.this).forceLoad();
 
 
 
 
 
-    }}
+
+
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<String>> onCreateLoader(int id, @Nullable Bundle args) {
+        progressBar.setVisibility(View.VISIBLE);
+        return new CurrencyLoader(this, CURRENCY_URL);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<String>> loader, ArrayList<String> currency) {
+        progressBar.setVisibility(View.GONE);
+        if (currency.size() == 0)
+            Toast.makeText(getApplicationContext(), "No Internet Connection size", Toast.LENGTH_SHORT).show();
+
+        ArrayAdapter<String> currencyAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.listitem, currency);
+
+
+        lv.setAdapter(currencyAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<String>> loader) {
+
+    }
+}
