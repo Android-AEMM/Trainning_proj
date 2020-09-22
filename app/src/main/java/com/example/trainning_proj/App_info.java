7ad68ed527c0b5e25e13a850e43c39f9c613bc53
emanwael <61898891+emanwael@users.noreply.github.com>
2020-09-23 00:23:03 +0200

    design

--- app/src/main/java/com/example/trainning_proj/App_info.java
+++ app/src/main/java/com/example/trainning_proj/App_info.java
@@ -3,12 +3,34 @@ package com.example.trainning_proj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class App_info extends AppCompatActivity {
 
+    TextView name_gamal,name_ehab,name_aya,name_eman,info_gamal,info_ehab,info_aya,info_eman,desc_app;
+    ImageView img_gamal, img_ehabe,img_aya,img_eman;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
+
+        name_gamal = findViewById(R.id.name_gamal);
+        name_ehab = findViewById(R.id.name_ehab);
+        name_aya = findViewById(R.id.name_aya);
+        name_eman = findViewById(R.id.name_eman);
+        info_aya = findViewById(R.id.info_aya);
+        info_ehab = findViewById(R.id.info_ehab);
+        info_eman = findViewById(R.id.info_eman);
+        info_gamal = findViewById(R.id.inf_gamal);
+        desc_app = findViewById(R.id.desc_app);
+        img_aya = findViewById(R.id.img_aya);
+        img_ehabe = findViewById(R.id.img_ehab);
+        img_eman = findViewById(R.id.img_eman);
+        img_gamal = findViewById(R.id.img_gamal);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/trainning_proj/Splash.java
+++ app/src/main/java/com/example/trainning_proj/Splash.java
@@ -11,17 +11,20 @@ import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
-    ImageView imageView;
-    Animation animation;
+    ImageView imageView,imageView_name;
+    Animation animation,animation1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         imageView = findViewById(R.id.image_splash);
+        imageView_name = findViewById(R.id.image_splash_name);
 
         animation = AnimationUtils.loadAnimation(Splash.this, R.anim.anim);
+        animation1 = AnimationUtils.loadAnimation(Splash.this, R.anim.anim1);
         imageView.startAnimation(animation);
+        imageView_name.startAnimation(animation1);
 
 
         // when you start user see it frist
